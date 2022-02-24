cc2a501efbf9960f6cb122a8441cc43983dc1ca0
Victoria Okosyan <resemble@rambler.ru>
2022-02-24 15:42:31 +0300

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs = Object.fromEntries(bracketsConfig); 
+  function getPair() {
+    if (i >= str.length) return false;
+    const close = pairs[str[i]]; 
+    if (!close) return false; 
+    for (i = i + 1; i < str.length; i++) {
+      if (str[i] === close) return true; 
+      if (!getPair()) return false; 
+    }
+    return false; 
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    if (!getPair()) return false; 
+  }
+  return true;
 }
